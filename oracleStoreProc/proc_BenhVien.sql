d87a70aa5eb371d97d82c5cb4cd0045723a05e54
KoahF <khoakg2013@gmail.com>
2021-04-08 10:32:20 +0630

    Add create_new_role procedure

--- oracleStoreProc/proc_BenhVien.sql
+++ oracleStoreProc/proc_BenhVien.sql
@@ -91,3 +91,34 @@ execute create_New_User('nhanvienkhambenh', '123');
 select *
 from dba_users
 order by created DESC
+
+--Tao role
+CL SCR;
+CREATE OR REPLACE PROCEDURE create_new_role (
+    pi_role_name NVARCHAR2
+)
+AS
+    li_count INT := 0;
+    existed_role EXCEPTION;
+    lv_stmt NVARCHAR2(200);
+BEGIN
+    select COUNT(1)
+    into li_count
+    from dba_roles
+    where role = pi_role_name;
+    
+    IF li_count != 0 THEN
+        RAISE existed_role;
+    END IF;
+    
+    lv_stmt := 'CREATE ROLE ' || pi_role_name || ' NOT IDENTIFIED';
+    EXECUTE IMMEDIATE(lv_stmt);
+EXCEPTION
+    WHEN existed_role THEN
+        raise_application_error(-20002, 'Role already existed');
+END;
+
+execute create_new_role('DucHuy');
+
+select *
+from dba_roles;
